c3eb10498ddeaa545d925a37886cd4198c26780c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-23 17:08:55 -0400

    Initial commit
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+chmod +x torch.sh && ./torch.sh
+pip install -r requirements.txt --upgrade
---
+++ torch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+pip install torch torchvision torchaudio torchtext --index-url https://download.pytorch.org/whl/cpu --upgrade
