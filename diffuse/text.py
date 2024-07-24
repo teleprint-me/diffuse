33d024b1f4c43cf0baae17abdabea5494d6a4f2b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-23 20:03:50 -0400

    refactor: Rename module from generate to text and remove image handling

--- diffuse/text.py
+++ diffuse/text.py
@@ -1,10 +1,9 @@
 """
-Module: diffuse.generate
+Module: diffuse.text
 """
 
 from datetime import datetime
 from time import sleep
-from typing import List, Optional, Tuple
 
 import numpy as np
 from diffusers.pipelines import DiffusionPipeline
@@ -58,46 +57,3 @@ def generate_text_to_image(
     elapsed_time = end_time - start_time
 
     return dataset, elapsed_time
-
-
-def generate_image_to_image(
-    pipe_image: DiffusionPipeline,
-    image_path: str,
-    prompt: str,
-    negative_prompt: Optional[str] = None,
-    strength=0.75,
-    num_inference_steps: int = 50,
-    guidance_scale=7.5,
-    num_images_per_prompt: int = 2,
-    output_directory: str = "images",
-    delay: float = 1 / 30,
-    dimensions: Optional[Tuple[int, int]] = None,
-) -> Tuple[List[Tuple[Image, str]], float]:
-    try:
-        start_time = datetime.now()
-        init_image = initialize_image(image_path, dimensions)
-
-        adjusted_inference_steps = adjust_inference_steps(strength, num_inference_steps)
-
-        result = pipe_image(
-            prompt=prompt,
-            image=init_image,
-            strength=strength,
-            num_inference_steps=adjusted_inference_steps,
-            guidance_scale=guidance_scale,
-            negative_prompt=negative_prompt,
-            num_images_per_prompt=num_images_per_prompt,
-        )
-
-        images = handle_pipeline_result(result)
-        dataset = write_images(images, output_directory, delay)
-
-        end_time = datetime.now()
-        elapsed_time = end_time - start_time
-
-        return dataset, elapsed_time
-
-    except KeyboardInterrupt:
-        # Gracefully interrupt image generation
-        print("KeyboardInterrupt: Exiting now.")
-        exit(1)
