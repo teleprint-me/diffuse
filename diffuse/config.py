e5740f474c4a27903af7128aea0dc3ebb040f226
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-25 14:36:33 -0400

    config(refactor): Generalize configuration pipeline keyword arguments

--- diffuse/config.py
+++ diffuse/config.py
@@ -8,17 +8,14 @@ import torch
 
 
 def config_pipeline(
-    use_single_file: bool = False,
-    use_safetensors: bool = True,
-    variant: str = "fp16",
-    torch_dtype: torch.device = torch.bfloat16,
     **kwargs: Dict[str, Any],
 ) -> Dict[str, Any]:
-    config = {
-        "use_single_file": use_single_file,
-        "use_safetensors": use_safetensors,
-        "variant": variant,
-        "torch_dtype": torch_dtype,
-    }
+    config = {}
+    config["variant"] = kwargs.get("variant", "fp16")
+    config["torch_dtype"] = kwargs.get("torch_dtype", torch.bfloat16)
+    config["use_safetensors"] = kwargs.get("use_safetensors", True)
+    config["add_watermarker"] = kwargs.get("add_watermarker", True)
+    config["use_single_file"] = kwargs.get("use_single_file", False)
+    config["device"] = kwargs.get("device", "cpu")
     config.update(kwargs)
     return config
