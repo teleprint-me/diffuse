8693e34c93129e6ada987856debda60becf82bac
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-25 04:06:21 -0400

    setup(chore): Remove unused dependency torchaudio from setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # install torch for cpu by default
-pip install torch torchvision torchaudio torchtext --index-url https://download.pytorch.org/whl/cpu --upgrade
+pip install torch torchvision torchtext --index-url https://download.pytorch.org/whl/cpu --upgrade
 # install required dependencies
 pip install -r requirements.txt --upgrade
